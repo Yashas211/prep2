c8eabffb9cbd8d15d98e0b4e3af7ce2b8eb0d894
Yashas211 <yashasbc21@gmail.com>
2026-06-18 11:11:11 +0530

    n

---
+++ src/test/java/WebpageTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WebpageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://yashas211.github.io/prep2/");
+    }git statu
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "DevOps Lab - GitHub Pages";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
